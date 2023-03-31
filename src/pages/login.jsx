67193cf696c73bb992cda60910cccd2c4fa1f4b9
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-03-31 16:54:28 +0300

    register

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 export default function Login() {
     return (
         <>
-            <h1 class="text-3xl font-bold text-center my-6">Welcome to the Blog App!</h1>
-            <p class="text-lg text-center my-4">This is a blog app that allows users to create, edit and delete posts.</p>
-            <form class="flex flex-col items-center">
-                <input type="text" placeholder='Username' class="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96" />
-                <input type="password" placeholder='Password' class="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96" />
-                <button class="bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600 transition-colors duration-300 ease-in-out mb-4 w-full md:w-40">Login</button>
-                <p class="text-center text-gray-700">Don't have an account yet ? <Link href="/register" class="text-blue-500 hover:underline">Register</Link></p>
+            <h1 className="text-3xl font-bold text-center my-6">Welcome to the Blog App!</h1>
+            <p className="text-lg text-center my-4">This is a blog app that allows users to create, edit and delete posts.</p>
+            <form className="flex flex-col items-center">
+                <input type="text" placeholder='Username' className="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96" />
+                <input type="password" placeholder='Password' className="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96" />
+                <button className="bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600 transition-colors duration-300 ease-in-out mb-4 w-full md:w-40">Login</button>
+                <p className="text-center text-gray-700">Don't have an account yet ? <Link href="/register" className="text-blue-500 hover:underline">Register</Link></p>
             </form>
         </> 
     )
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,17 +1,41 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function register() {
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState([]);
+
+    const register = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch("http://localhost:8800/api/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username, password })
+        })
+        const error = await response.json()
+        if(error) return setError(error)
+
+    }
     return (
-<>
-    <h1 class="text-3xl font-bold text-center my-6">Register for the Blog App!</h1>
-    <form class="flex flex-col items-center">
-        <input type="text" placeholder='Username' class="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96" />
-        <input type="password" placeholder='Password' class="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96" />
-        <input type="password" placeholder='Confirm Password' class="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96" />
-        <button class="bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600 transition-colors duration-300 ease-in-out mb-4 w-full md:w-40">Register</button>
-        <p class="text-center text-gray-700">Already have an account? <Link href="/login" class="text-blue-500 hover:underline">Login</Link></p>
-    </form>
-</>
+        <>
+            <form className="flex flex-col items-center" onSubmit={register}>
+                <h1 className="text-3xl font-bold text-center my-6">Register for the Blog App!</h1>
+                <input type="text" placeholder='Username' className="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96"
+                    value={username}
+                    onChange={e => setUserName(e.target.value)}
+                />
+                <input type="password" placeholder='Password' className="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                {error && <p className=''>{error}</p>}
+                <button className="bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600 transition-colors duration-300 ease-in-out mb-4 w-full md:w-40">Register</button>
+                <p className="text-center text-gray-700">Already have an account? <Link href="/login" className="text-blue-500 hover:underline">Login</Link></p>
+            </form>
+        </>
     )
 }
