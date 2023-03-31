21e6d0daeb9b0326ff75be729fb90a48a60fd574
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-03-31 17:23:50 +0300

    handle error and info messages

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,25 +1,38 @@
 import Link from 'next/link'
 import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 
 export default function register() {
     const [username, setUserName] = useState("");
     const [password, setPassword] = useState("");
-    const [error, setError] = useState([]);
-
+    const [info, setInfo] = useState("");
+    const [error, setError] = useState("");
+    const router = useRouter();
+   
     const register = async (e) => {
         e.preventDefault();
-
-        const response = await fetch("http://localhost:8800/api/auth/register", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ username, password })
-        })
-        const error = await response.json()
-        if(error) return setError(error)
-
+        try {
+            const response = await fetch("http://localhost:8800/api/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ username, password })
+            })
+            const data = await response.json();
+            if (response.ok) {
+                setInfo(data);
+                setTimeout(() =>{
+                    router.push("/login");
+                },1500);
+            } else {
+                setError(data);
+            }
+        } catch (err) {
+            setError(err.message)
+        }
     }
+    
     return (
         <>
             <form className="flex flex-col items-center" onSubmit={register}>
@@ -32,6 +45,7 @@ export default function register() {
                     value={password}
                     onChange={e => setPassword(e.target.value)}
                 />
+                {info && <p className=''>{info}</p>}
                 {error && <p className=''>{error}</p>}
                 <button className="bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600 transition-colors duration-300 ease-in-out mb-4 w-full md:w-40">Register</button>
                 <p className="text-center text-gray-700">Already have an account? <Link href="/login" className="text-blue-500 hover:underline">Login</Link></p>
