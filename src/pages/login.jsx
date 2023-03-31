9e621ea457e3e8a0d06de06bda4a848274e24ca4
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-03-31 18:15:14 +0300

    handle login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,17 +1,42 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:8800/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include",
+                body: JSON.stringify({ username, password })
+            })
+        } catch(err){
+            console.log(err)
+        }
+    }
+
     return (
         <>
             <h1 className="text-3xl font-bold text-center my-6">Welcome to the Blog App!</h1>
             <p className="text-lg text-center my-4">This is a blog app that allows users to create, edit and delete posts.</p>
-            <form className="flex flex-col items-center">
-                <input type="text" placeholder='Username' className="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96" />
-                <input type="password" placeholder='Password' className="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96" />
+            <form className="flex flex-col items-center" onSubmit={login}>
+                <input type="text" placeholder='Username' className="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96"
+                    value={username}
+                    onChange={(e) => setUserName(e.target.value)}
+                />
+                <input type="password" placeholder='Password' className="border border-gray-400 rounded-lg py-2 px-3 mb-4 w-full md:w-96"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <button className="bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600 transition-colors duration-300 ease-in-out mb-4 w-full md:w-40">Login</button>
                 <p className="text-center text-gray-700">Don't have an account yet ? <Link href="/register" className="text-blue-500 hover:underline">Register</Link></p>
             </form>
-        </> 
+        </>
     )
 }
