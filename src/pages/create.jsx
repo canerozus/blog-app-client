5da4df23be0cc193b970bef6722f774f42701232
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-04-06 15:54:56 +0300

    react-quill changes

--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -1,37 +1,66 @@
 import { useState, useMemo } from "react";
 import "react-quill/dist/quill.snow.css";
 import dynamic from "next/dynamic";
+
+const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+        ['link', 'image'],
+        ['clean']
+    ],
+}
+const formats =
+    [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image'
+    ]
+
 export default function CreatePost() {
-    const [value, setValue] = useState("");
-    const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), { ssr: false }),[]);
-  
+    const [title, setTitle] = useState("")
+    const [summary, setSummary] = useState("")
+    const [content, setContent] = useState("");
+    const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), { ssr: false }), []);
+
     return (
-<form className="max-w-2xl mx-auto bg-white rounded-lg overflow-hidden shadow-lg">
-  <div className="px-4 py-2 bg-gray-100 font-bold text-lg">Create a New Post</div>
-  <div className="p-4">
-    <div className="mb-4">
-      <label className="block text-gray-700 font-bold mb-2" htmlFor="title">Title</label>
-      <input className="w-full p-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-500" type="text" id="title" name="title" placeholder="Enter a title for your post" />
-    </div>
-    <div className="mb-4">
-      <label className="block text-gray-700 font-bold mb-2" htmlFor="summary">Summary</label>
-      <textarea className="w-full p-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-500" id="summary" name="summary" placeholder="Enter a summary for your post"></textarea>
-    </div>
-    <div className="mb-4">
-      <label className="block text-gray-700 font-bold mb-2" htmlFor="image-file">Image File</label>
-      <input className="w-full p-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-500" type="file" id="image-file" name="image-file" />
-    </div>
-    <div className="mb-4">
-      <label className="block text-gray-700 font-bold mb-2" htmlFor="content">Content</label>
-      <div className=" rounded-md mb-6 pb-12">
-        <ReactQuill className="h-56" theme="snow" value={value} onChange={setValue} />
-      </div>
-      <div className="flex justify-end">
-        <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none" type="submit">Create Post</button>
-      </div>
-    </div>
-  </div>
-</form>
+        <form className="max-w-2xl mx-auto bg-white rounded-lg overflow-hidden shadow-lg">
+            <div className="px-4 py-2 bg-gray-100 font-bold text-lg">Create a New Post</div>
+            <div className="p-4">
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="title">Title</label>
+                    <input className="w-full p-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-500" type="text" id="title" name="title" placeholder="Enter a title for your post"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="summary">Summary</label>
+                    <input className="w-full p-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-500" id="summary" name="summary" placeholder="Enter a summary for your post"
+                        value={summary}
+                        onChange={(e) => setSummary(e.target.value)}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="image-file">Image File</label>
+                    <input className="w-full p-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-500" type="file" id="image-file" name="image-file" />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="content">Content</label>
+                    <div className=" rounded-md mb-6 pb-12">
+                        <ReactQuill className="h-56" theme="snow" modules={modules} formats={formats}
+                            value={content}
+                            onChange={(newValue) => setContent(newValue)}
+                        />
+                    </div>
+                    <div className="flex justify-end">
+                        <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none" type="submit">Create Post</button>
+                    </div>
+                </div>
+            </div>
+        </form>
 
     )
 }
