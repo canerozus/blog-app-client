0067e0d519c99865e8cc1a85fe34f6e548b2763f
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-04-12 18:00:01 +0300

    final push

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,7 +8,7 @@ export default function Post({_id, title, summary, cover, createdAt, author }) {
     <div className='flex flex-col '>
         <Link href={`/post/${_id}`} >
       <div className='flex gap-2 m-3 w-full flex-col sm:flex-row sm:items-start items-center'>
-          <img className='sm:w-2/6 object-fit h-64' src={'http://localhost:8800/' + cover} alt="" />
+          <img className='sm:w-2/6 w-3/6 object-fit h-64' src={'http://localhost:8800/' + cover} alt="" />
         <div className='w-4/6 flex flex-col gap-5  text-start  mx-2 my-0 '>
           <h2 className='text-3xl font-bold overflow-x-hidden overflow-y-auto max-h-20 p-2'>{title}</h2>
           <div className='w-full px-2'>
